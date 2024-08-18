2736ab78e9078469e0f4556f44a9421a3695bdf9
Jenith1304 <jenithpanchal1304@gmail.com>
2024-08-18 22:27:22 +0530

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages'; // Only if you installed vite-plugin-gh-pages
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
+  plugins: [
+    react(),
+    ghPages() // Only if you installed vite-plugin-gh-pages
+  ],
+  base: '/DiceGameReact/', // Replace 'your-repo-name' with your actual GitHub repository name
+  build: {
+    outDir: 'build', // GitHub Pages serves from the `build` or `docs` folder by default
+  },
 });
